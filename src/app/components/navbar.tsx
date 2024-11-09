82a804ab5feac284aeb5f43086cee95e2271a462
786haider <haiderhussain536@gmail.com>
2024-11-09 03:27:28 -0800

    third commit

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,6 @@
-"use client"
 
-import Typewriter from 'typewriter-effect';
+
+// import Typewriter from 'typewriter-effect';
 import ProfilePic from '../../../public/profile.jpg'
 import Image from 'next/image'
 
@@ -9,6 +9,7 @@ export default function Navbar(){
     return (
         <div className="bg-cyan-500 " >
         <nav className="mt-0 flex" style={{height:'80px'}}>
+        
             <Image
             src={ProfilePic}
             alt='Profile Picture'
@@ -16,21 +17,28 @@ export default function Navbar(){
             height={0}
             className='border rounded-full mt-1 ml-3'
             />
-<h1 className='text-3xl mt-5 ml-3 font-extrabold'>
+<h1 className='text-xl md:text-3xl mt-5 ml-3 font-extrabold text-violet-950'>
    
    <i>
     <u>
+    Haider Hussain`s Portfolio !
          
-<Typewriter
+{/* <Typewriter
   options={{
-    strings: ['Haider Hussain`s Portfolio'],
+    strings: ['Haider Hussain`s Portfolio !'],
     autoStart: true,
     loop: true,
   }}
-/>
+/> */}
     </u>
    </i>
 </h1>
+
+<ul className='flex justify-center text-xl md:text-2xl mt-6 ml-80 font-semibold space-x-16 '>
+  <li className='hover:text-3xl hover:text-wrap hover:text-pink-500'>Home</li>
+  <li className='hover:text-3xl hover:text-wrap hover:text-pink-500'>Contact</li>
+  <li className='hover:text-3xl hover:text-wrap hover:text-pink-500'>About</li>
+</ul>
       
         </nav>
         </div>
